2b870626bb52d8f4622dd0828d61d17bc386f092
jenniferlong8 <jlong8@bu.edu>
2018-07-03 14:30:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine,name) {
+  var position = katzDeliLine.length + 1 
   for (let i=1; i < katzDeliLine.length; i++) {
     katzDeliLine.push(`Welcome, ${name}. You are number ${i} in line.`)
   }
