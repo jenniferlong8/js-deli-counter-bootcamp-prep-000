b022e07888c635795c1d969ab2d6b5df829747f0
jenniferlong8 <jlong8@bu.edu>
2018-07-03 15:28:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var line = [];
 function currentLine(katzDeliLine) {
   let i = 0;
   while (i < katzDeliLine.length) {
-    line.
+    line.push(` ` + [i+1] + `. ` + katzDeliLine[i])
   }
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
