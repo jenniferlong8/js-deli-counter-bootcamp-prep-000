77e5b5d1379bbab1b93ca0970ecc8be9e16943a5
jenniferlong8 <jlong8@bu.edu>
2018-07-03 15:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function takeANumber(katzDeliLine,name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
-  else {
+  } else {
     return `Currently serving ${katzDeliLine[0]}.`
-    katzDeliLine.shift
+    katzDeliLine.shift()
   }
   }
 }
