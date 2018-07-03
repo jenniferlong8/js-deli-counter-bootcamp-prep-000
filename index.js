90320cb2745437b106aed30caa66187649f5e2a4
jenniferlong8 <jlong8@bu.edu>
2018-07-03 15:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine) {
     return "The line is currently empty."
   else {
     return `Currently serving ${katzDeliLine[0]}.`
-    delete katzDeliLine[0]
+    katzDeliLine.shift
   }
   }
 }
