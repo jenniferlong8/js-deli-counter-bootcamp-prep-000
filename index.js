23530defee9fd5537fc41502e645044bc37d5e1b
jenniferlong8 <jlong8@bu.edu>
2018-07-03 15:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine,name) {
 }
 
 function nowServing(katzDeliLine) {
-
+  name = name[1]
     return `Currently serving ${name}.`
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
